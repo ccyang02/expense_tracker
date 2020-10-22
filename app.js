9d2e2b7e06c520e91b65b4f348045a36f42a5690
ccyang02 <chiachingyang02@gmail.com>
2020-10-23 01:34:08 +0800

    feat: get data from two collections for index page

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
@@ -7,6 +9,17 @@ const routes = require('./routes')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(routes)
+app.use(bodyParser.urlencoded({ extended: true }))
+
+mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}.`)
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,12 +1,32 @@
 const express = require('express')
 const router = express.Router()
+const bodyParser = require('body-parser')
+const Record = require('../../models/record.js')
+const Category = require('../../models/category.js')
+
+router.use(bodyParser.urlencoded({ extended: true }))
 
 router.get('/', (req, res) => {
   return res.redirect('/index')
 })
 
 router.get('/index', (req, res) => {
-  return res.render('index')
+  const queryCate = req.query.category
+  const promises = []
+
+  let condition = (queryCate) ? { category: queryCate } : {}
+
+  promises.push(Category.find().sort({ '_id': 'asc' }).lean().exec())
+  promises.push(Record.find(condition).lean().exec())
+
+  Promise.all(promises).then(results => {
+    const categories = results[0]
+    const records = results[1]
+    // console.log(records[0].date.getMonth())
+    return res.render('index', { categories, records })
+  }).catch(err => {
+    console.log(err)
+  })
 })
 
 module.exports = router
